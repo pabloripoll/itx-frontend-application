ecaf288e2b954e167480bc4bcb487815612af29a
Pablo Ripoll <pabloripoll.it@gmail.com>
2026-04-02 16:54:29 +0200

    feat: enviroment variables

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getCache, setCache } from '../service/cacheService';
 
-const BASE_URL = 'https://itx-frontend-test.onrender.com';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
     baseURL: BASE_URL,
